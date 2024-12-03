ed46684fd83e8e577d06087620b3123d92a54faa
TimMindat <timmind@aiesec.net>
2024-12-03 03:12:31 +0200

    fix3

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,13 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  optimizeDeps: {
-    exclude: ['lucide-react'],
+  server: {
+    port: 3000,
+    open: true
   },
+  optimizeDeps: {
+    include: ['react', 'react-dom', 'react-router-dom', 'i18next', 'react-i18next']
+  }
 });
