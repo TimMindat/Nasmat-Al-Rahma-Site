22c154b699e14f92c546e04301f069c0196abd7e
TimMindat <timmind@aiesec.net>
2024-12-04 05:57:19 +0200

    11

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
 import Layout from './components/Layout';
 import AppRoutes from './routes';
-import './utils/i18n';
 
 function App() {
   return (
